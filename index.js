ddf5486adc1d97bd52a2e51a11e78d2334fc09eb
Fabian Tschirschnitz <fabian.tschirschnitz@sap.com>
2016-04-29 18:12:26 +0200

    fooo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cloudinary = require('cloudinary');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -13,8 +14,17 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 });
 
+app.post('/', function(req, res) {
+    response.send(JSON.stringify(request))
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
+cloudinary.config({cloud_name: 'dqkznq6ix',api_key: '557366327443484',api_secret: 'oXkowgZov8g5pa-YXirUpoFqdRI'});
+
+cloudinary.uploader.upload("test.jpg", function(result) { console.log(result)});
 
 
